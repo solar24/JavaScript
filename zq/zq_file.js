0483c0e3da0bef56bb79c218195a7b7463f44750
zhanghail <zhanghail@topband.com.cn>
2021-11-12 16:59:44 +0800

    中青

---
+++ zq/zq_file.js
@@ -0,0 +1,37 @@
+/*
+    中青文件管理
+ */
+
+const zq_cookie_file = 'zq_cookie.txt'
+
+const zq_withdraw_file = 'zq_withdraw.txt'
+
+const zq_box_file = 'zq_box.txt'
+
+const zq_kkz_file = 'zq_kkz.txt'
+
+const zq_llz_file = 'zq_llz.txt'
+
+const zq_ssz_file = 'zq_ssz.txt'
+
+const zq_qd_file = 'zq_qd.txt'
+
+const zq_wz_file = 'zq_wz.txt'
+
+const zq_wz_time_file = 'zq_wz_time.txt'
+
+const zq_5_60_file = 'zq_5_60.txt'
+
+
+
+
+exports.zq_cookie_file = zq_cookie_file
+exports.zq_withdraw_file = zq_withdraw_file
+exports.zq_box_file = zq_box_file
+exports.zq_kkz_file = zq_kkz_file
+exports.zq_llz_file = zq_llz_file
+exports.zq_ssz_file = zq_ssz_file
+exports.zq_qd_file = zq_qd_file
+exports.zq_wz_file = zq_wz_file
+exports.zq_wz_time_file = zq_wz_time_file
+exports.zq_5_60_file = zq_5_60_file
